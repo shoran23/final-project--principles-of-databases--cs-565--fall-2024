c11a724635bc5890e89bf6646304a9b04ad3a22f
shoran23 <scottf.horan@gmail.com>
2024-12-15 21:02:36 -0500

    added to the update account script so the input type would change based on the attribute selection

--- includes/scripts/updateAccount.js
+++ includes/scripts/updateAccount.js
@@ -2,11 +2,24 @@ const selector = document.getElementById("update-account-current-attribute");
 selector.addEventListener("change", (e) => {
     const usernameSelector = document.getElementById("update-account-attribute-username");
     const attributeInput = document.getElementById("update-account-attribute-input");
-    if(e.target.value === "username") {
-        usernameSelector.disabled = false;
-        attributeInput.disabled = true;
-    } else {
-        usernameSelector.disabled = true;
-        attributeInput.disabled = false;
+    switch(e.target.value) {
+        case "username": {
+            usernameSelector.disabled = false;
+            attributeInput.disabled = true;
+            attributeInput.type = "text";
+            attributeInput.value = "";
+            break;
+        }
+        case "password": {
+            usernameSelector.disabled = true;
+            attributeInput.disabled = false;
+            attributeInput.type = "password";
+            break;
+        }
+        default: {
+            usernameSelector.disabled = true;
+            attributeInput.disabled = false;
+            attributeInput.type = "text";
+        }
     }
 });
