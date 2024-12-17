91de43d610bae4e89fa343c757c8e91051b85857
shoran23 <scottf.horan@gmail.com>
2024-12-16 20:49:13 -0500

    removed the scripts directory and javascript final and embedded the script directly in the php code for update accounts

--- includes/php/update-account-form.php
+++ includes/php/update-account-form.php
@@ -30,4 +30,31 @@
         <input type="hidden" name="submitted" value="6">
         <p><input type="submit" value="update-account"></p>
     </fieldset>
+    <script>
+        const selector = document.getElementById("update-account-current-attribute");
+        selector.addEventListener("change", (e) => {
+            const usernameSelector = document.getElementById("update-account-attribute-username");
+            const attributeInput = document.getElementById("update-account-attribute-input");
+            switch(e.target.value) {
+                case "username": {
+                    usernameSelector.disabled = false;
+                    attributeInput.disabled = true;
+                    attributeInput.type = "text";
+                    attributeInput.value = "";
+                    break;
+                }
+                case "password": {
+                    usernameSelector.disabled = true;
+                    attributeInput.disabled = false;
+                    attributeInput.type = "password";
+                    break;
+                }
+                default: {
+                    usernameSelector.disabled = true;
+                    attributeInput.disabled = false;
+                    attributeInput.type = "text";
+                }
+            }
+        });
+    </script>
 </form>
